563ed9ad3081c293631dc9e78b8fab7f6b026908
arsyanmw <arsyanmw12@gmail.com>
2022-03-05 01:37:12 +0700

    add status field

--- src/models/hutang.js
+++ src/models/hutang.js
@@ -14,6 +14,11 @@ const TransaksiHutang = new Schema({
         type : Number,
         required : true
     },
+    status: {
+        type : Number,
+        required : true,
+        default : 0
+    },
 }, {
     timestamps : true
 });
