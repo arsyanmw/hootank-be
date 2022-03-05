bb63452d96fd0aa651f0bca42c37039d69ad8f7e
arsyanmw <arsyanmw12@gmail.com>
2022-03-05 18:26:29 +0700

    things

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const sudahBayarRoutes = require('./src/routes/sudahBayar');
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
@@ -19,16 +19,16 @@ app.use(bodyParser.json());
 app.use('/hutang', hutangRoutes);
 app.use('/sudah-bayar', sudahBayarRoutes);
 
-// app.use((err, req, res) => {
-//     const status = err.statusShop || 500;
-//     const message = err.message;
-//     const data = err.data;
-//
-//     res.status(status).json({
-//         message: message,
-//         data: data
-//     });
-// });
+app.use((err, req, res) => {
+    const status = err.statusShop || 500;
+    const message = err.message;
+    const data = err.data;
+
+    res.status(status).json({
+        message: message,
+        data: data
+    });
+});
 
 mongoose.connect('mongodb+srv://arsyan:FdjMdxQ60Rs76AQh@cluster0.dufwc.mongodb.net/hutang?retryWrites=true&w=majority')
     .then(() => {
