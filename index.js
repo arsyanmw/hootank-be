49a8b74d4b7e6b39121b3ea23fba3f207b118e58
arsyanmw <arsyanmw12@gmail.com>
2022-03-05 18:04:06 +0700

    update: allow all header

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const sudahBayarRoutes = require('./src/routes/sudahBayar');
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
 });
 
